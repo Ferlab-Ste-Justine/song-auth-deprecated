394005152d3aa055f4993fc92652cec8f6f5328b
Eric Vallee <eric_vallee@webificservices.com>
2019-12-13 15:42:45 -0500

    bug fix

--- src/proxy.js
+++ src/proxy.js
@@ -1,6 +1,10 @@
 var httpProxy = require('http-proxy');
 var proxy = httpProxy.createProxyServer({});
 
+proxy.on('proxyReq', function(proxyReq, req, res, options) {
+    proxyReq.removeHeader('authorization')
+});
+
 const proxy_request = (target) => ((req, res) => {  
     proxy.web(req, res, { target })
 })
