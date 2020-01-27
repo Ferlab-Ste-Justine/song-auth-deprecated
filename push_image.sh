863f4b409ca2f136090fa3ef300c7e0663045203
Eric Vallee <eric_vallee@webificservices.com>
2020-01-27 14:08:13 -0500

    Changed push tag

--- push_image.sh
+++ push_image.sh
@@ -1,6 +1,6 @@
 export NAME=$(cat ./src/package.json | jq -r ".name")
 export VERSION=$(cat ./src/package.json | jq -r ".version")
-export IMAGE=chusj/$NAME:$VERSION
+export IMAGE=chusj/$NAME:clin-qa-$VERSION
 
 (cd src; docker build -t $IMAGE .)
 
